56c185ad024c18f0195d7f8310d1d4c02aeb53c8
ChristopherGabba <chrisgabba2005@yahoo.com>
2025-01-10 11:33:52 -0600

    test compile

--- amplify/storage/resource.ts
+++ amplify/storage/resource.ts
@@ -1,5 +1,11 @@
 import { defineStorage } from '@aws-amplify/backend';
 
 export const storage = defineStorage({
-  name: 'testStorageForCompressorBug'
+  name: 'testStorageForCompressorBug',
+  access: (allow) => ({
+    'public/*': [
+      allow.guest.to(['read', "delete", "write"]),
+      allow.authenticated.to(['read', 'write', 'delete']),
+    ],
+  })
 });
